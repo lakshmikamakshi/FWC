8ac1fb94ddffac74fe9e81d9b70d698bd8faf4eb
lakshmikamakshi <111626486+lakshmikamakshi@users.noreply.github.com>
2022-08-31 23:36:55 +0530

    Update main.asm

--- assignment_asm/main.asm
+++ assignment_asm/main.asm
@@ -10,21 +10,24 @@ main:
   mov r19,r17
   ldi r18,0b00000001
   and r19,r18            ; x = r19
-  mov r20,r17
+  mov r16,r17
   ldi r18,0b00000010
-  and r20,r18       
-  lsr r20        ;y=r20
-  mov r21,r17
+  and r16,r18
+  ldi r23, 0b00000001
+  rcall loopw
+  mov r20,r16;y=r20
+  mov r16,r17
   ldi r18,0b00000100
-  and r21,r18   
-  lsr r21
-  lsr r21   ;z=r21
-  mov r22,r17
+  and r16,r18   
+  ldi r23,0b00000010
+  rcall loopw
+  mov r21,r16;z=r21
+  mov r16,r17
   ldi r18,0b00001000
-  and r22,r18  
-  lsr r22
-  lsr r22
-  lsr r22      ;w=r22 
+  and r16,r18  
+  ldi r23,0b00000011
+  rcall loopw
+  mov r22,r16  ;w=r22 
   ldi r18,0b00000001
   eor r22,r18          ;W'
   and r22,r21          ;z&&w'
@@ -36,12 +39,21 @@ main:
   and r19,r21          ;x'z'
   or r19,r20
   or r19,r22           ;final exp
-  lsl r19
-  lsl r19 
+  ldi r23,0b00000010
+  rcall loopv
   ldi r16,0b00000100
   out DDRD ,r16
   out PORTD,r19  
 
   rjmp main
-
+loopw:
+lsr r16
+dec r23
+brne loopw
+ret
+loopw:
+lsl r19
+dec r23
+brne loopw
+ret
 
